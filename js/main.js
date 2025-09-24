520e498b05d545e105b5205332f871d4ac75798d
namnc <nam.nguyen.chan@gmail.com>
2025-09-24 21:45:05 +0700

    Change contact link to #

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ function loadHeader() {
                 navLinks = `
                     <a href="${basePath}index.html" class="nav-link">Trang chủ</a>
                     <a href="${basePath}product-list/product-list.html" class="nav-link">Sản phẩm</a>
-                    <a href="${basePath}contact/contact.html" class="nav-link">Liên hệ</a>
+                    <a href="#" class="nav-link">Liên hệ</a>
                     <a href="${basePath}my-account/my-account.html" class="nav-link">Tài khoản của tôi (${currentUser.name})</a>
                     <a href="#" id="logout-link" class="nav-link">Đăng xuất</a>
                     <a href="${basePath}cart/cart.html" class="nav-link cart-link">
